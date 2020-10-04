e39ece4c86d8d76b20baa591425ad1317a16f702
juan <juanfri_tentor@hotmail.com>
2020-10-04 20:50:10 -0300

    Agregadas y configuradas dependencias

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var mqtt=require('mqtt');
+var cors = require('cors');
+var app = express();
+var PORT = 3000;
+app.use(express.json());
+
+var corsConfig = { origin: '*', optionesSucessStatus: 200 };
+app.use(cors(corsConfig));
+
+app.listen(PORT, function (req, res) {
+	console.log('API Funcionando ');
+});
