dc4a21e791965cbea631a592302c056a6b065b36
juan <juanfri_tentor@hotmail.com>
2020-10-23 16:12:33 -0300

    modificado archivo de arranque run

--- run.sh
+++ run.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
+cd backend
+echo "service mosquitto stop"
+sudo service mosquitto stop
+echo "Cerrando contenedores que esten corriendo"
+docker-compose down &
+sleep 5
+echo "Levantando conenedores"
+docker-compose up
 
-echo "Cerrando posibles contenedores abiertos"
-docker stop $(docker ps -a -q)
-echo "Eliminando red"
-docker network rm mysql-net &
-sleep 5
-echo "Creando red"
-docker network create --driver bridge mysql-net &
-sleep 5
-cd Api-nodejs
-echo "Iniciando base de datos mysql"
-./start_mysql.sh mysql-net "$PWD"/db &
-sleep 5
-echo "Iniciando phpadmin en puerto 8085"
-./run_phpadmin.sh mysql-net mysql-server 8085 &
-sleep 5
-# echo "Iniciando nodejs en puerto 3000"
-# ./serve_node_app_net.sh "$PWD" index.js 3000 mysql-net &
-# sleep 5
-# cd ../ionicProject
+# cd ../fronted
 # echo "Instalando librerias necesarias"
 # npm install
-# echo "Levantando aplicacion ionic"
-# ionic serve
+# echo "Levantando aplicacion vue"
+# npm run serve
