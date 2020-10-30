f73952abadf0987c233f359ac2fac5369e0f4449
juan <juanfri_tentor@hotmail.com>
2020-10-30 11:24:32 -0300

    Agregado de sensores cuando se agrega un dispositivo

--- backend/routes/dispositivo/index.js
+++ backend/routes/dispositivo/index.js
@@ -17,22 +17,21 @@ routerDispositivo.get('/', function (req, res) {
 
 // obtengo lista de dispositivo de un usuario
 routerDispositivo.get('/:idUsuario', function (req, res) {
-	pool.query('Select * from Dispositivos where idUsuario=? and conectado=1', [req.params.idUsuario], function (
-		err,
-		result,
-		fields
-	) {
-		if (err) {
-			res.send(err).status(400);
-			return;
+	pool.query(
+		'Select * from Dispositivos where idUsuario=? and conectado=1',
+		[req.params.idUsuario],
+		function (err, result, fields) {
+			if (err) {
+				res.send(err).status(400);
+				return;
+			}
+			res.send(result);
 		}
-		res.send(result);
-	});
+	);
 });
 
 // agrego dispositivo
 routerDispositivo.post('/agregar', function (req, res) {
-	console.log(req.body);
 	let current_datetime = moment().utcOffset('-0300').format('YYYY-MM-DD HH:mm:ss');
 
 	pool.query(
@@ -52,6 +51,59 @@ routerDispositivo.post('/agregar', function (req, res) {
 				res.send(err).status(400);
 				return;
 			}
+
+			let idDispositivo = result.insertId;
+
+			pool.query(
+				'INSERT INTO Sensores (uuidSensor,tipo, nombre, ubicacion, conectado, fecha_creacion, fecha_actualizacion,codigo, unidad, idDispositivo) VALUES ?',
+				[
+					[
+						[
+							`${req.body.uuid}_1`,
+							'sensor',
+							'Humedad',
+							req.body.ubicacion,
+							1,
+							current_datetime,
+							current_datetime,
+							11,
+							'%',
+							idDispositivo,
+						],
+						[
+							`${req.body.uuid}_2`,
+							'sensor',
+							'Temperatura',
+							req.body.ubicacion,
+							1,
+							current_datetime,
+							current_datetime,
+							22,
+							'C',
+							idDispositivo,
+						],
+						[
+							`${req.body.uuid}_3`,
+							'sensor',
+							'Actuador',
+							req.body.ubicacion,
+							1,
+							current_datetime,
+							current_datetime,
+							33,
+							'C',
+							idDispositivo,
+						],
+					],
+				],
+				function (err, result, fields) {
+					if (err) {
+						res.send(err).status(400);
+						return;
+					}
+				}
+			);
+
 			res.send(result);
 		}
 	);
