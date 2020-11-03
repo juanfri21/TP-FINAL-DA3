5313025707d161a6dbd65cfa98eff8a67f6f3c0d
juan <juanfri_tentor@hotmail.com>
2020-11-02 23:59:17 -0300

    Funcionando script para levantar proyecto

--- run.sh
+++ run.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 cd backend
 echo "service mosquitto stop"
-service mosquitto stop &
+sudo service mosquitto stop &
+sleep 5
+echo "Instalando librerias necesarias para nodejs"
+npm install &
 sleep 5
 echo "Cerrando contenedores que esten corriendo"
 docker-compose down &
 sleep 5
 echo "Levantando conenedores"
-docker-compose up
-
-# cd ../fronted
-# echo "Instalando librerias necesarias"
-# npm install
-# echo "Levantando aplicacion vue"
-# npm run serve
+docker-compose up &
+sleep 15
+cd ../fronted/iot-system
+echo "Instalando librerias necesarias para vue.js"
+npm install
+echo "Levantando aplicacion vue"
+npm run serve
